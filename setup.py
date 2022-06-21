9cd3e34a519f848862cc4cdb984ff0ce0cd78821
iroha_nano <49903762+strelka145@users.noreply.github.com>
2022-06-21 16:46:11 +0900

    version up
    version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plasmidrender",
-    version="1.0.8",
+    version="1.0.9",
     author="strelka",
     author_email="irohaprg@gmail.com",
     description="CLI tool to illustrate plasmids with a structure described by json.",
